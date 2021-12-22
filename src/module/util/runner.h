37a4a3587f482263bea40c15557a92419cd8ff63
huqinghong <huqinghong@uniontech.com>
2021-12-22 16:12:30 +0800

    fix: 增加runner.h执行异常时错误信息打印
    增加runner.h中Runner执行异常时错误信息打印
    
    Log:
    Change-Id: I14ba7f4f495ae8e07823cb18b441b7702752d7fc

--- src/module/util/runner.h
+++ src/module/util/runner.h
@@ -42,8 +42,8 @@ auto Runner(const T program = "ostree", const Y args = "", const P timeout = 300
     auto retStatus = runner.exitStatus();
     auto retCode = runner.exitCode();
     if ((retStatus != 0) || (retStatus == 0 && retCode != 0)) {
-        qCritical() << program << " run failed, retCode:" << retCode;
-        qCritical() << program << " err msg:" << runner.readAllStandardOutput();
+        qCritical() << program << " run failed, retCode:" << retCode << ", info msg:" << runner.readAllStandardOutput()
+                    << ", err msg:" << runner.readAllStandardError();
         return false;
     }
     return true;

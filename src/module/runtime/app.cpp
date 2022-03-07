f2fcd6ce5af269aec98ea3c4c3d2ae1e20e49a35
chenhuixing <chenhuixing@uniontech.com>
2022-03-07 14:51:34 +0800

    fix: add env variables for QtWebEngine
    Make QtWebEngine use the new path under runtime by environment variables.
    
    Log:
    Change-Id: Iea2c18f709cacc7a65cfec0439c8a996bcc7429a

--- src/module/runtime/app.cpp
+++ src/module/runtime/app.cpp
@@ -254,6 +254,8 @@ public:
         r->process->env.push_back("LD_LIBRARY_PATH=" + fixLdLibraryPath.join(":"));
         r->process->env.push_back("QT_PLUGIN_PATH=/runtime/lib/x86_64-linux-gnu/qt5/plugins:/usr/lib/x86_64-linux-gnu/qt5/plugins");
         r->process->env.push_back("QT_QPA_PLATFORM_PLUGIN_PATH=/runtime/lib/x86_64-linux-gnu/qt5/plugins/platforms:/usr/lib/x86_64-linux-gnu/qt5/plugins/platforms");
+        r->process->env.push_back("QTWEBENGINEPROCESS_PATH=/runtime/lib/x86_64-linux-gnu/qt5/libexec");
+        r->process->env.push_back("QTWEBENGINERESOURCE_PATH=/runtime/share/qt5/translations:/runtime/share/qt5/resources");
         return 0;
     }
 

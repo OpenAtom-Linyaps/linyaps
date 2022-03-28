4413e79d8fdd5dc8e7a7312fca52034145794497
Heysion <heysion@deepin.com>
2022-03-28 11:24:58 +0800

    feat: add testcase for info header file
    给package/info.h 添加单元测试案例
    
    Log:
    Change-Id: I5c965636dd9b1764643c2386cbe38e9864bde892

---
+++ test/module/package/package_info_test.cpp
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2022. Uniontech Software Ltd. All rights reserved.
+ *
+ * Author:     Heysion <heysion@deepin.com>
+ *
+ * Maintainer: Heysion <heysion@deepin.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include <gtest/gtest.h>
+#include <QJsonDocument>
+#include <QFile>
+#include <QDebug>
+
+#include "src/module/package/package.h"
+#include "src/module/package/info.h"
+#include "src/module/util/xdg.h"
+
+TEST(PermissionTest, LoadJson)
+{
+    QStringList userTypeList = linglong::util::getXdgUserDir();
+
+    EXPECT_NE(userTypeList.size(), 0);
+
+    linglong::package::registerAllMetaType();
+
+    QFile jsonFile("../../test/data/demo/info.json");
+    jsonFile.open(QIODevice::ReadOnly);
+
+    auto json = QJsonDocument::fromJson(jsonFile.readAll());
+
+    auto variant = json.toVariant();
+
+    EXPECT_NE(variant, "");
+
+    auto info = variant.value<linglong::package::Info *>();
+
+    EXPECT_NE(info, nullptr);
+
+    auto userStaticMount = info->permissions->filesystem->user;
+    EXPECT_NE(userStaticMount, nullptr);
+
+    auto userVariant = toVariant<linglong::package::User>(userStaticMount);
+
+    auto userStaticMap = userVariant.toMap();
+
+    EXPECT_NE(userStaticMap.size(), 0);
+
+    // load r/rw key to map
+    QMap<QString, QString> userStaticMountMap;
+    for (const auto &it : userStaticMap.keys()) {
+        auto itValue = userStaticMap.value(it).toString();
+        if (itValue != "" && (itValue == "r" || itValue == "rw")) {
+            userStaticMountMap.insert(it, itValue);
+        }
+    }
+
+    // foreach show r/rw key
+    for (const auto &key : userStaticMountMap.keys()) {
+        qInfo() << key;
+        qInfo() << "userStaticMountMap.value(key):" << userStaticMountMap.value(key);
+        if (userTypeList.indexOf(key) != -1
+            && (userStaticMountMap.value(key) == "r " || userStaticMountMap.value(key) == "rw")) {
+            EXPECT_TRUE(true);
+        }
+        EXPECT_NE(userTypeList.indexOf(key), -1);
+    }
+
+    info->deleteLater();
+}
+
+using namespace linglong::package;
+
+TEST(PermissionTest, TestPermission)
+{
+    linglong::package::registerAllMetaType();
+
+    // load json file
+    QFile jsonFile("../../test/data/demo/info.json");
+    jsonFile.open(QIODevice::ReadOnly);
+
+    auto json = QJsonDocument::fromJson(jsonFile.readAll());
+
+    QVariant variant = json.toVariant();
+
+    EXPECT_NE(variant, "");
+
+    // convert json to info
+    auto r = variant.value<Info *>();
+
+    EXPECT_NE(r, nullptr);
+
+    // check app info
+    EXPECT_EQ(r->appid, "cn.wps.wps-office");
+    EXPECT_EQ(r->version, "11.1.0.10161");
+    EXPECT_EQ(r->arch, QStringList({"amd64"}));
+    EXPECT_EQ(r->kind, "");
+    EXPECT_EQ(r->name, "wps-office");
+    EXPECT_EQ(r->description, "");
+    EXPECT_EQ(r->runtime, "");
+    EXPECT_EQ(r->base, "");
+
+    // get user permission
+    auto permission = r->permissions;
+
+    EXPECT_NE(permission, nullptr);
+
+    // check permission
+    EXPECT_EQ(permission->autostart, true);
+    EXPECT_EQ(permission->notification, true);
+    EXPECT_EQ(permission->trayicon, true);
+    EXPECT_EQ(permission->clipboard, true);
+    EXPECT_EQ(permission->account, true);
+    EXPECT_EQ(permission->bluetooth, true);
+    EXPECT_EQ(permission->camera, true);
+    EXPECT_EQ(permission->audioRecord, true);
+    EXPECT_EQ(permission->installedApps, true);
+
+    // check user permission
+    EXPECT_NE(permission->filesystem, nullptr);
+    EXPECT_NE(permission->filesystem->user, nullptr);
+
+    // check user permission with template
+    EXPECT_EQ(permission->filesystem->user->desktop, "rw");
+    EXPECT_EQ(permission->filesystem->user->documents, "r");
+    EXPECT_EQ(permission->filesystem->user->downloads, "");
+    EXPECT_EQ(permission->filesystem->user->music, "");
+    EXPECT_EQ(permission->filesystem->user->pictures, "");
+    EXPECT_EQ(permission->filesystem->user->videos, "");
+    EXPECT_EQ(permission->filesystem->user->templates, "");
+    EXPECT_EQ(permission->filesystem->user->temp, "");
+    EXPECT_EQ(permission->filesystem->user->publicShare, "");
+
+    r->deleteLater();
+}

5d40b6a80e3eb55f3c784c265473e93f41e26a66
yuanqiliang <yuanqiliang@uniontech.com>
2022-04-15 20:52:32 +0800

    feat: Doxygen生成pdf接口文档
    Log:
    Change-Id: I3451aae681a736ed6b589bdd09e7119d95d7724b

--- README.md
+++ README.md
@@ -74,3 +74,49 @@ use gcov、lcov to generate convert html report, make shure at the top level of 
 ```bash
 ./code_coverage.sh
 ```
+
+## Doxygen生成pdf接口文档
+1. 安装中文latex环境
+```bash
+sudo apt-get install doxygen graphviz texlive-full latex-cjk-chinese* cjk-latex
+```
+2. 生成配置文件
+```bash
+Doxygen –g
+```
+3. 打开Doxyfile，递归遍历当前目录的子目录，寻找被文档化的程序源文件，修改如下：
+```plain
+RECURSIVE              = YES
+```
+4. 文档生成
+```bash
+doxygen Doxyfile
+```
+5.进入到latex目录
+```bash
+cd latex
+```
+6. 修改前面doxygen Doxyfile生成的latex文件：refman.tex
+将其中的:
+```plain
+\begin{document}
+```
+改为：
+```plain
+\usepackage{CJKutf8}
+\begin{document}
+\begin{CJK}{UTF8}{gbsn}
+```
+并将其中的:
+```plain
+\end{document}
+```
+改为：
+```plain
+\end{CJK}
+\end{document}
+```
+8. 生成pdf文件
+```bash
+make -j
+```
--- src/service/impl/package_manager.h
+++ src/service/impl/package_manager.h
@@ -27,20 +27,37 @@
 #include "package_manager_flatpak_impl.h"
 #include "package_manager_option.h"
 
-class PackageManagerPrivate;
+class PackageManagerPrivate;    /**< forward declaration PackageManagerPrivate */
+
+/**
+ * @brief The PackageManager class
+ * @details PackageManager is a singleton class, and it is used to manage the package
+ *          information.
+ */
 class PackageManager
     : public QObject
     , protected QDBusContext
     , public linglong::util::Singleton<PackageManager>
 {
     Q_OBJECT
-    Q_CLASSINFO("D-Bus Interface", "com.deepin.linglong.PackageManager")
+    Q_CLASSINFO("D-Bus Interface", "com.deepin.linglong.PackageManager")    /**< D-Bus Interface */
 
-    friend class linglong::util::Singleton<PackageManager>;
+    friend class linglong::util::Singleton<PackageManager>;   /**< friend class */
 
 public Q_SLOTS:
-    QString Status();
+    QString Status();   /**< get the status of package manager */
 
+    /**
+     * @brief download the package
+     * 
+     * @param packageIdList 玲珑包ID列表
+     * @param savePath 下载存储路径
+     * @return RetMessageList 消息列表 
+     *          state:true or false 
+     *          code:状态码(0:成功,1:失败)
+     *          message:消息
+     *
+     */
     RetMessageList Download(const QStringList &packageIdList, const QString savePath);
     RetMessageList Install(const QStringList &packageIdList, const ParamStringMap &paramMap = {});
     RetMessageList Uninstall(const QStringList &packageIdList, const ParamStringMap &paramMap = {});

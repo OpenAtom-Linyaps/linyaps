25a9bd577af27675f664545340a6883b72e86438
yuanqiliang <yuanqiliang@uniontech.com>
2022-04-18 11:09:43 +0800

    refactor: PackageManager接口文档完善
    Log:
    Change-Id: I4f91a32e0e35bc3171dd32d47a05475b55a746b1

--- src/service/impl/package_manager.h
+++ src/service/impl/package_manager.h
@@ -40,40 +40,107 @@ class PackageManager
     , public linglong::util::Singleton<PackageManager>
 {
     Q_OBJECT
-    Q_CLASSINFO("D-Bus Interface", "com.deepin.linglong.PackageManager")    /**< D-Bus Interface */
+    Q_CLASSINFO("D-Bus Interface", "com.deepin.linglong.PackageManager")
 
-    friend class linglong::util::Singleton<PackageManager>;   /**< friend class */
+    friend class linglong::util::Singleton<PackageManager>;
 
 public Q_SLOTS:
-    QString Status();   /**< get the status of package manager */
+    /**
+     * @brief get status of package manager
+     * @return
+     */
+    QString Status();
 
     /**
      * @brief download the package
      * 
      * @param packageIdList 玲珑包ID列表
      * @param savePath 下载存储路径
-     * @return RetMessageList 消息列表 
-     *          state:true or false 
-     *          code:状态码(0:成功,1:失败)
+     * @return RetMessageList 消息列表 \n
+     *          state:true or false \n
+     *          code:状态码(0:成功,1:失败) \n
      *          message:消息
      *
      */
     RetMessageList Download(const QStringList &packageIdList, const QString savePath);
+
+    /**
+     * @brief 
+     * 
+     * @param packageIdList 
+     * @param paramMap 
+     * @return RetMessageList 
+     */
     RetMessageList Install(const QStringList &packageIdList, const ParamStringMap &paramMap = {});
+
+    /**
+     * @brief 
+     * 
+     * @param packageIdList 
+     * @param paramMap 
+     * @return RetMessageList 
+     */
     RetMessageList Uninstall(const QStringList &packageIdList, const ParamStringMap &paramMap = {});
+
+    /**
+     * @brief 
+     * 
+     * @param packageIdList 
+     * @param paramMap 
+     * @return RetMessageList 
+     */
     RetMessageList Update(const QStringList &packageIdList, const ParamStringMap &paramMap = {});
+
+    /**
+     * @brief 
+     * 
+     * @return QString 
+     */
     QString UpdateAll();
 
+    /**
+     * @brief 
+     * 
+     * @param packageIdList 
+     * @param paramMap 
+     * @return AppMetaInfoList 
+     */
     AppMetaInfoList Query(const QStringList &packageIdList, const ParamStringMap &paramMap = {});
 
+    /**
+     * @brief 
+     * 
+     * @param packageIdList 
+     * @return QString 
+     */
     QString Import(const QStringList &packagePathList);
 
+    /**
+     * @brief 
+     * 
+     * @param packageId 
+     * @param paramMap 
+     * @return RetMessageList 
+     */
     RetMessageList Start(const QString &packageId, const ParamStringMap &paramMap = {});
+
+    /**
+     * @brief 
+     * 
+     * @param containerId 
+     * @return RetMessageList 
+     */
     RetMessageList Stop(const QString &containerId);
+
+    /**
+     * @brief 
+     * 
+     * @return ContainerList 
+     */
     ContainerList ListContainer();
 
 private:
-    QScopedPointer<PackageManagerPrivate> dd_ptr;
+    QScopedPointer<PackageManagerPrivate> dd_ptr;   ///< private data
     Q_DECLARE_PRIVATE_D(qGetPtrHelper(dd_ptr), PackageManager)
 
 protected:

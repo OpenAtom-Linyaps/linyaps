f6152062c90272c730d73419d3eacde5a3e5af67
yuanqiliang <yuanqiliang@uniontech.com>
2022-04-27 17:17:54 +0800

    refactor: 删除冗余代码
    Log:
    Change-Id: I2e445895dd446407acf92d7705d44fffaab62599

--- src/service/impl/dbus_retcode.h
+++ src/service/impl/dbus_retcode.h
@@ -9,52 +9,18 @@
  */
 
 #pragma once
-// uap  error code
-#define UAP_INSTALL_TRUE 400 //安装正确
-#define FILE_NO_EXISTS_ERROR 401 //文件不存在
-#define DIR_NO_EXISTS_ERROR 402 //目录不存在
-#define UAP_NAME_ERROR 403 // uap包名格式错误
-#define UAP_NO_EXISTS_ERROR 404 // uap包不存在
-#define UAP_INSTALL_ERROR 405 // uap 安转失败
 
 namespace linglong {
 namespace dbus {
 enum class RetCode {
-    uap_install_success = 400, // 安装正确
-    DataMakeSquashfsSuccess, //mksquashfs success
-    uap_install_file_not_exists = 401, // 文件不存在
-    uap_install_directory_not_exists, // 目录不存在
-    uap_install_failed, // uap 安转失败
-    uap_name_format_error = 501, // uap包名格式错误
-    uap_file_not_exists, // uap包不存在
-    DataDirNotExists,      //数据目录不存在
-    UapJsonFileNotExists,  //uap.json不存在
-    UapJsonFormatError,      //uap.json format error
-    DataEntriesDirNotExists,     //entries 目录不存在
-    DataFilesDirNotExists,     //files 目录不存在
-    DataInfoJsonNotExists,     //info.json 文件不存在
-    DataMakeSquashfsError,      //mksquashfs failed
-    MakeTempWorkDirError,       //制作临时目录失败
-    BundleFileNotExists,           //uab文件不存在
-    MakeUapFailed,              //制作uap包失败
-    MakeOuapFailed,             //制作ouap包失败
-    PushBundleFailed,           //推送uab包失败
-
+    DataDirNotExists, //数据目录不存在
+    UapJsonFileNotExists, // uap.json不存在
+    BundleFileNotExists, // uab文件不存在
     user_input_param_err = 600,
-    user_env_list_err,
     pkg_already_installed,
     pkg_not_installed,
-    host_arch_not_recognized,
-    update_appstream_failed,
     install_runtime_failed,
-    search_pkg_by_appstream_failed,
-    load_ouap_file_failed,
     load_pkg_data_failed,
-    extract_ouap_failed,
-    ouap_install_file_not_exists, // 文件不存在
-    ouap_install_failed,
-    ouap_download_success,
-    ouap_install_success, // ouap文件安装成功
     pkg_install_success, // 安装成功
     pkg_install_failed, // 安装失败
     pkg_uninstall_success, // 卸载成功
@@ -66,16 +32,15 @@ enum class RetCode {
     ErrorPkgQuerySuccess, // 查询成功
     ErrorPkgQueryFailed // 查询失败
 };
-template <typename T=int>
-inline T RetCode(RetCode code) {
+
+template<typename T = int>
+inline T RetCode(RetCode code)
+{
     return static_cast<T>(code);
 }
 } // namespace dbus
 
 namespace Status {
-enum StatusCode {
-    SUCCESS = 0,
-    FAIL = 1
-};
+enum StatusCode { SUCCESS = 0, FAIL = 1 };
 }
 } // namespace linglong

de81beb35c1fa61c894c027ee3e893a38c6bcc95
chenlinxuan <chenlinxuan@uniontech.com>
2022-08-04 17:50:28 +0800

    fix: fix profile script error
      /etc/os-release can be anything, might not have "VERSION_ID" in it.
      So after sed, $VERSION can be empty string, or even a string with
      space inside.
    
      We should always quote variable when we using it.
    
    Change-Id: I14046270ef446c9f878c66336150735c44038dd0

--- scripts/linglong.sh
+++ scripts/linglong.sh
@@ -3,37 +3,37 @@
 LINGLONG_ROOT="/var/lib/linglong"
 VERSION=0
 
-if [ -f /etc/os-release ]
-then
-        VERSION=$(sed -ne '/^VERSION_ID=.*$/P' /etc/os-release | awk -F = '{print $2}' | sed -e 's/"//g' | awk -F . '{print $1}')
-        if [ ${VERSION} -eq 20 ]
-        then
-                LINGLONG_ROOT="/data/linglong"
-        elif [ ${VERSION} -eq 23 ]
-        then
-                LINGLONG_ROOT="/persistent/linglong"
-        fi
+if [ -f /etc/os-release ]; then
+	VERSION=$(sed -ne '/^VERSION_ID=.*$/P' /etc/os-release | awk -F = '{print $2}' | sed -e 's/"//g' | awk -F . '{print $1}')
+	if [ -z "${VERSION}" ]; then
+		VERSION=0
+	fi
+	if [ ${VERSION} -eq 20 ]; then
+		LINGLONG_ROOT="/data/linglong"
+	elif [ ${VERSION} -eq 23 ]; then
+		LINGLONG_ROOT="/persistent/linglong"
+	fi
 fi
 
 SYS_LINGLONG_INSTALLATIONS="${LINGLONG_ROOT}/entries/share"
 USR_LINGLONG_INSTALLATIONS="${XDG_DATA_HOME:-$HOME/.local/share}/linglong/entries/share"
 
 case ":${XDG_DATA_DIRS}:" in
-	*":${USR_LINGLONG_INSTALLATIONS}:"*)
-		;;
-	*)
-		export XDG_DATA_DIRS
-		XDG_DATA_DIRS="${USR_LINGLONG_INSTALLATIONS}:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
-		export LINGLONG_ROOT
-		;;
+*":${USR_LINGLONG_INSTALLATIONS}:"*) ;;
+
+*)
+	export XDG_DATA_DIRS
+	XDG_DATA_DIRS="${USR_LINGLONG_INSTALLATIONS}:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
+	export LINGLONG_ROOT
+	;;
 esac
 
 case ":${XDG_DATA_DIRS}:" in
-	*":${SYS_LINGLONG_INSTALLATIONS}:"*)
-		;;
-	*)
-		export XDG_DATA_DIRS
-		XDG_DATA_DIRS="${SYS_LINGLONG_INSTALLATIONS}:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
-		export LINGLONG_ROOT
-		;;
+*":${SYS_LINGLONG_INSTALLATIONS}:"*) ;;
+
+*)
+	export XDG_DATA_DIRS
+	XDG_DATA_DIRS="${SYS_LINGLONG_INSTALLATIONS}:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
+	export LINGLONG_ROOT
+	;;
 esac

6fc025e7c8f4478561aa4c533a23a2260502d9bf
black-desk <me@black-desk.cn>
2023-08-11 16:08:43 +0800

    chore: add SPDX-* comments to tools/scripts/*
    Signed-off-by: black-desk <me@black-desk.cn>

--- tools/scripts/update-external.sh
+++ tools/scripts/update-external.sh
@@ -1,5 +1,9 @@
 #!/bin/env bash
 
+# SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
 cd "$(git rev-parse --show-toplevel)" || exit 255
 
 tmpdir=$(

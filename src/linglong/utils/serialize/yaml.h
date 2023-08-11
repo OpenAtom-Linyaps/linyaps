b27847272bb2e35094225c0f0873ccb7081dc61e
black-desk <me@black-desk.cn>
2023-08-11 14:10:41 +0800

    chore: add SPDX-* comments to src/* and apps/*
    Signed-off-by: black-desk <me@black-desk.cn>

--- src/linglong/utils/serialize/yaml.h
+++ src/linglong/utils/serialize/yaml.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
 #ifndef LINGLONG_UTILS_SERIALIZE_YAML_H
 #define LINGLONG_UTILS_SERIALIZE_YAML_H
 

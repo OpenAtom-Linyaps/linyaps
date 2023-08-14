51fb1b043c0f23f7d6e8d1768b7aa96131fe5089
black-desk <me@black-desk.cn>
2023-08-14 21:20:49 +0800

    chore: add licenses for tests
    Signed-off-by: black-desk <me@black-desk.cn>

--- src/linglong/adaptors/system_helper/package_manager_helper1.cpp
+++ src/linglong/adaptors/system_helper/package_manager_helper1.cpp
@@ -1,1 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
 #include "linglong/adaptors/system_helper/package_manager_helper1.h"

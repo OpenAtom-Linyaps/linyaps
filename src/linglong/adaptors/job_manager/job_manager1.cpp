66760ac0847a72239af018d41e604c09ab2e8e59
black-desk <me@black-desk.cn>
2023-08-14 21:20:49 +0800

    chore: add licenses for tests
    Signed-off-by: black-desk <me@black-desk.cn>

---
+++ src/linglong/adaptors/job_manager/job_manager1.cpp
@@ -0,0 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+#include "linglong/adaptors/job_manager/job_manager1.h"
--- tests/utils/serialize/TestStruct.h
+++ tests/utils/serialize/TestStruct.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
 #ifndef LINGLONG_TEST_UTILS_SERIALIZE_TESTSTRUCT_H
 #define LINGLONG_TEST_UTILS_SERIALIZE_TESTSTRUCT_H
 

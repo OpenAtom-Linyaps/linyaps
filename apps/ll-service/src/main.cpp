0771d234026026afdb95993f24ac1f3db41fad06
black-desk <me@black-desk.cn>
2023-08-14 18:22:58 +0800

    build: mv app_manager adaptor to linglong/adaptors
    Signed-off-by: black-desk <me@black-desk.cn>

--- apps/ll-service/src/main.cpp
+++ apps/ll-service/src/main.cpp
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
-#include "dbusgen/AppManagerAdaptor.h"
+#include "linglong/adaptors/app_manager/app_manager1.h"
 #include "linglong/dbus_ipc/workaround.h"
 
 #include <QCoreApplication>
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    AppManager1Adaptor pma(APP_MANAGER);
+    linglong::adaptors::app_manger::AppManager1 pma(APP_MANAGER);
 
     // TODO(se): 需要进行错误处理
     dbus.registerObject("/org/deepin/linglong/AppManager", APP_MANAGER);

7b360baa21e5feb50078acd146c1bae80477efbb
black-desk <me@black-desk.cn>
2023-10-15 03:26:47 +0800

    chore(tools/generate-coverage): enable ccache
    Signed-off-by: black-desk <me@black-desk.cn>

--- tools/generate-coverage.sh
+++ tools/generate-coverage.sh
@@ -5,21 +5,25 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
 set -x
+set -e
 
 cd "$(git rev-parse --show-toplevel)" || exit 255
 
-tmpdir=$(
-        mktemp -d build-generate-coverage-XXXXXX || exit 255
-)
+builddir=build-generate-coverage
 
 export CXXFLAGS="$CXXFLAGS --coverage -g -O0"
 
-cmake -B "$tmpdir" -S . || exit 255
+command -v ccache &>/dev/null && {
+        USE_CCACHE=-DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+}
 
-cmake --build "$tmpdir" -j "$(nproc)" || exit 255
+# shellcheck disable=SC2086
+cmake --fresh -B "$builddir" -S . $USE_CCACHE || exit 255
 
-cmake --build "$tmpdir" -t test
+cmake --build "$builddir" -j "$(nproc)" || exit 255
 
-mkdir "$tmpdir"/report || exit 255
+cmake --build "$builddir" -t test
 
-gcovr --filter "src/*" --html-nested "$tmpdir"/report/index.html
+mkdir -p "$builddir"/report || exit 255
+
+gcovr --filter "src/*" --html-nested "$builddir"/report/index.html

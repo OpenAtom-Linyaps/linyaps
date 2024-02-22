b6c160bba3b973366130dd2f72d9d681e0a46278
black-desk <me@black-desk.cn>
2024-02-22 10:49:31 +0800

    chore: update PFL.cmake and ocppi
    Signed-off-by: black-desk <me@black-desk.cn>

--- cmake/PFL.cmake
+++ cmake/PFL.cmake
@@ -12,7 +12,7 @@ if(PFL_INITIALIZED)
   return()
 endif()
 
-set_property(GLOBAL PROPERTY PFL_INITIALIZED true)
+message(STATUS "PFL: --==Version: v0.2.12==--")
 
 # You should call this function in your top level CMakeLists.txt to tell
 # PFL.cmake some global settings about your project.
@@ -23,10 +23,8 @@ set_property(GLOBAL PROPERTY PFL_INITIALIZED true)
 #
 # EXTERNALS list of string: External projects to build.
 function(pfl_init)
-        cmake_parse_arguments(PFL_INIT "" "INSTALL;ENABLE_TESTING;BUILD_EXAMPLES" "EXTERNALS"
-                        ${ARGN})
-
-  message(STATUS "PFL: Version 0.1.0")
+  cmake_parse_arguments(PFL_INIT "" "INSTALL;ENABLE_TESTING;BUILD_EXAMPLES"
+                        "EXTERNALS" ${ARGN})
 
   set(PFL_ENABLE_TESTING
       ${PFL_INIT_ENABLE_TESTING}
@@ -35,8 +33,12 @@ function(pfl_init)
       ${PFL_INIT_BUILD_EXAMPLES}
       PARENT_SCOPE)
   set(PFL_INSTALL
-          ${PFL_INIT_INSTALL}
+      ${PFL_INIT_INSTALL}
       PARENT_SCOPE)
+  set(PFL_PREFIX
+      ${PROJECT_NAME}
+      PARENT_SCOPE)
+
   foreach(EXTERNAL ${PFL_INIT_EXTERNALS})
     add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/external/${EXTERNAL})
   endforeach()
@@ -50,10 +52,10 @@ function(__pfl_configure_files)
 
     configure_file(${IN_FILE} ${CMAKE_CURRENT_BINARY_DIR}/${OUT_FILE} @ONLY)
 
-    if(OUT_FILE MATCHES "^include\/.*\.h(h|pp)?$")
+    if(OUT_FILE MATCHES "^(./)?include\/.*\.h(h|pp)?$")
       list(APPEND ${PFL_CONFIGURE_FILES_HEADERS}
            ${CMAKE_CURRENT_BINARY_DIR}/${OUT_FILE})
-    elseif(OUT_FILE MATCHES "^src\/.*\.(h(h|pp)?|cpp)$")
+    elseif(OUT_FILE MATCHES "^(./)?src\/.*\.(h(h|pp)?|cpp)$")
       list(APPEND ${PFL_CONFIGURE_FILES_SOURCES}
            ${CMAKE_CURRENT_BINARY_DIR}/${OUT_FILE})
     else()
@@ -73,63 +75,66 @@ endfunction()
 # This function is used to add libraries under the `libs` directory. It just
 # takes in a string list contains the directory name under `libs`.
 function(pfl_add_libraries)
-  cmake_parse_arguments(PFL_ADD_LIBRARIES "" "VERSION" "LIBS" ${ARGN})
+  cmake_parse_arguments(PFL_ADD_LIBRARIES "" "" "LIBS" ${ARGN})
 
-  message(
-    STATUS
-      "PFL:${PFL_MESSAGE_INDENT} Adding libraries at ${CMAKE_CURRENT_SOURCE_DIR}"
-  )
+  message(STATUS "PFL: Adding libraries at ${CMAKE_CURRENT_SOURCE_DIR}")
 
-  set(PFL_MESSAGE_INDENT "${PFL_MESSAGE_INDENT}  ")
-
-  cmake_path(GET CMAKE_CURRENT_SOURCE_DIR FILENAME TARGET_DIR_NAME)
-
-  if("${CMAKE_CURRENT_SOURCE_DIR}" STREQUAL "${PROJECT_SOURCE_DIR}")
-    set(TARGET_DIR_NAME ${PROJECT_NAME})
-  endif()
-
-  string(REPLACE " " "_" TARGET_NAME "${TARGET_DIR_NAME}")
-
-  if(PFL_PREFIX)
-    set(PFL_PREFIX "${PFL_PREFIX}::${TARGET_NAME}")
-  else()
-    set(PFL_PREFIX "${TARGET_NAME}")
+  if(NOT "${CMAKE_CURRENT_SOURCE_DIR}" STREQUAL "${PROJECT_SOURCE_DIR}")
+    message(
+      FATAL_ERROR
+        "PFL: pfl_add_libraries can only be called in \$PROJECT_SOURCE_DIR")
   endif()
 
   foreach(LIB ${PFL_ADD_LIBRARIES_LIBS})
     add_subdirectory(libs/${LIB})
   endforeach()
 
-  if (PFL_INSTALL)
-          return()
+  if(NOT PFL_INSTALL)
+    return()
   endif()
 
   include(GNUInstallDirs)
 
-  if(EXISTS
-     ${CMAKE_CURRENT_SOURCE_DIR}/misc/cmake/${TARGET_NAME}-config.cmake.in)
-    include(CMakePackageConfigHelpers)
-    # This will be used to replace @PACKAGE_cmakeModulesDir@
-    set(cmakeModulesDir cmake)
-
-    configure_package_config_file(
-      misc/cmake/${TARGET_NAME}-config.cmake.in
-      misc/cmake/${TARGET_NAME}-config.cmake
-      INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${TARGET_NAME}
-      PATH_VARS cmakeModulesDir
-      NO_SET_AND_CHECK_MACRO NO_CHECK_REQUIRED_COMPONENTS_MACRO)
-
-    write_basic_package_version_file(
-      ${CMAKE_CURRENT_BINARY_DIR}/misc/cmake/${TARGET_NAME}-config-version.cmake
-      VERSION ${PFL_ADD_LIBRARIES_VERSION}
-      COMPATIBILITY SameMajorVersion)
-
-    install(
-      FILES
-        ${CMAKE_CURRENT_BINARY_DIR}/misc/cmake/${TARGET_NAME}-config.cmake
-        ${CMAKE_CURRENT_BINARY_DIR}/misc/cmake/${TARGET_NAME}-config-version.cmake
-      DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${TARGET_NAME})
+  string(TOLOWER "${PROJECT_NAME}" LOWER_PROJECT_NAME)
+  set(CONFIG_FILE misc/cmake/${LOWER_PROJECT_NAME}-config.cmake)
+  set(VERSION_FILE misc/cmake/${LOWER_PROJECT_NAME}-config-version.cmake)
+  if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${CONFIG_FILE}.in)
+    set(CONFIG_FILE misc/cmake/${PROJECT_NAME}Config.cmake)
+    set(VERSION_FILE misc/cmake/${LOWER_PROJECT_NAME}ConfigVersion.cmake)
   endif()
+
+  if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${CONFIG_FILE}.in)
+    message(STATUS "PFL: tried ")
+    message(
+      WARNING
+        "PFL: tried ${CMAKE_CURRENT_SOURCE_DIR}/misc/cmake/${PROJECT_NAME}Config.cmake.in"
+    )
+    message(
+      FATAL_ERROR
+        "PFL: cmake config file not found."
+        "${CMAKE_CURRENT_SOURCE_DIR}/misc/cmake/${LOWER_PROJECT_NAME}-config.cmake.in"
+        "${CMAKE_CURRENT_SOURCE_DIR}/misc/cmake/${PROJECT_NAME}Config.cmake.in")
+  endif()
+
+  include(CMakePackageConfigHelpers)
+  # This will be used to replace @PACKAGE_cmakeModulesDir@
+  set(cmakeModulesDir cmake)
+
+  configure_package_config_file(
+    ${CMAKE_CURRENT_SOURCE_DIR}/${CONFIG_FILE}.in
+    ${CMAKE_CURRENT_BINARY_DIR}/${CONFIG_FILE}
+    INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}
+    PATH_VARS cmakeModulesDir
+    NO_SET_AND_CHECK_MACRO NO_CHECK_REQUIRED_COMPONENTS_MACRO)
+
+  write_basic_package_version_file(
+    ${VERSION_FILE}
+    VERSION ${PFL_ADD_LIBRARIES_VERSION}
+    COMPATIBILITY SameMajorVersion)
+
+  install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${CONFIG_FILE}
+                ${CMAKE_CURRENT_BINARY_DIR}/${VERSION_FILE}
+          DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME})
 endfunction()
 
 # This function is used to add a library.
@@ -166,25 +171,37 @@ function(pfl_add_library)
 
   cmake_path(GET CMAKE_CURRENT_SOURCE_DIR FILENAME TARGET_DIR_NAME)
 
+  if(TARGET_DIR_NAME MATCHES ".*__.*")
+    message(
+      FATAL_ERROR "Invalid directory name ${TARGET_DIR_NAME} contains '__'.")
+  endif()
+
   if("${CMAKE_CURRENT_SOURCE_DIR}" STREQUAL "${PROJECT_SOURCE_DIR}")
     set(TARGET_DIR_NAME ${PROJECT_NAME})
+    if(NOT PFL_ADD_LIBRARY_OUTPUT_NAME)
+      set(PFL_ADD_LIBRARY_OUTPUT_NAME ${PROJECT_NAME})
+    endif()
   endif()
 
   string(REPLACE " " "_" TARGET_NAME "${TARGET_DIR_NAME}")
 
   string(REPLACE "::" "__" TARGET_PREFIX "${PFL_PREFIX}")
 
-  if(TARGET_PREFIX)
-    set(TARGET_NAME "${TARGET_PREFIX}__${TARGET_NAME}")
-  endif()
+  set(TARGET_NAME "${TARGET_PREFIX}__${TARGET_NAME}")
+  string(REPLACE "__" "::" TARGET_ALIAS "${TARGET_NAME}")
 
-  string(REPLACE "__" "::" TARGET_EXPORT_NAME "${TARGET_NAME}")
+  if(NOT PFL_ADD_LIBRARY_OUTPUT_NAME)
+    set(PFL_ADD_LIBRARY_OUTPUT_NAME ${TARGET_NAME})
+    message(
+      WARNING
+        "PFL: OUTPUT_NAME of ${TARGET_ALIAS} not set, using ${PFL_ADD_LIBRARY_OUTPUT_NAME}"
+    )
+  endif()
 
   message(
     STATUS
-      "PFL:${PFL_MESSAGE_INDENT} Adding library ${TARGET_EXPORT_NAME} at ${CMAKE_CURRENT_SOURCE_DIR}"
+      "PFL: Adding library ${PFL_ADD_LIBRARY_OUTPUT_NAME} as ${TARGET_ALIAS} at ${CMAKE_CURRENT_SOURCE_DIR}"
   )
-  set(PFL_MESSAGE_INDENT "${PFL_MESSAGE_INDENT}  ")
 
   __pfl_configure_files(INS ${PFL_ADD_LIBRARY_INS} HEADERS
                         PFL_ADD_LIBRARY_HEADERS SOURCES PFL_ADD_LIBRARY_SOURCES)
@@ -196,24 +213,21 @@ function(pfl_add_library)
   else()
     add_library("${TARGET_NAME}" ${PFL_ADD_LIBRARY_TYPE}
                                  ${PFL_ADD_LIBRARY_SOURCES})
+    if(NOT "${PFL_ADD_LIBRARY_SOVERSION}" STREQUAL "")
+      set_target_properties("${TARGET_NAME}"
+                            PROPERTIES SOVERSION ${PFL_ADD_LIBRARY_SOVERSION})
+    endif()
   endif()
 
-  if(PFL_ADD_LIBRARY_SOVERSION AND NOT PFL_ADD_LIBRARY_HEADER_ONLY)
-    set_target_properties("${TARGET_NAME}"
-                          PROPERTIES SOVERSION ${PFL_ADD_LIBRARY_SOVERSION})
-  endif()
-
-  if(NOT "${TARGET_EXPORT_NAME}" STREQUAL "${TARGET_NAME}")
-    add_library("${TARGET_EXPORT_NAME}" ALIAS "${TARGET_NAME}")
+  if(NOT "${TARGET_ALIAS}" STREQUAL "${TARGET_NAME}")
+    add_library("${TARGET_ALIAS}" ALIAS "${TARGET_NAME}")
   endif()
 
   set_target_properties("${TARGET_NAME}" PROPERTIES EXPORT_NAME
                                                     ${TARGET_DIR_NAME})
 
-  if(PFL_ADD_LIBRARY_OUTPUT_NAME)
-    set_target_properties("${TARGET_NAME}"
-                          PROPERTIES OUTPUT_NAME ${PFL_ADD_LIBRARY_OUTPUT_NAME})
-  endif()
+  set_target_properties("${TARGET_NAME}"
+                        PROPERTIES OUTPUT_NAME ${PFL_ADD_LIBRARY_OUTPUT_NAME})
 
   target_sources(
     ${TARGET_NAME}
@@ -242,45 +256,62 @@ function(pfl_add_library)
   if(PFL_ADD_LIBRARY_COMPILE_FEATURES)
     target_compile_features(${TARGET_NAME} ${PFL_ADD_LIBRARY_COMPILE_FEATURES})
   endif()
-        
+
   if(PFL_INSTALL AND NOT PFL_ADD_LIBRARY_INTERNAL)
     include(GNUInstallDirs)
 
+    set(EXPORT_NAME ${PFL_ADD_LIBRARY_OUTPUT_NAME})
+    if(NOT EXPORT_NAME)
+      set(EXPORT_NAME ${TARGET_NAME})
+    endif()
+
     install(
       TARGETS "${TARGET_NAME}"
-      EXPORT "${TARGET_NAME}"
+      EXPORT "${EXPORT_NAME}"
       FILE_SET HEADERS)
 
     install(
-      EXPORT "${TARGET_NAME}"
+      EXPORT "${EXPORT_NAME}"
       DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}
-      FILE "${TARGET_NAME}.cmake"
+      FILE "${EXPORT_NAME}.cmake"
       NAMESPACE ${PFL_PREFIX}::)
 
-    if(EXISTS
-       ${CMAKE_CURRENT_SOURCE_DIR}/misc/cmake/${TARGET_NAME}-config.cmake.in)
-      include(CMakePackageConfigHelpers)
-      # This will be used to replace @PACKAGE_cmakeModulesDir@
-      set(cmakeModulesDir cmake)
-
-      configure_package_config_file(
-        misc/cmake/${TARGET_NAME}-config.cmake.in
-        misc/cmake/${TARGET_NAME}-config.cmake
-        INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${TARGET_NAME}
-        PATH_VARS cmakeModulesDir
-        NO_SET_AND_CHECK_MACRO NO_CHECK_REQUIRED_COMPONENTS_MACRO)
-
-      write_basic_package_version_file(
-        ${CMAKE_CURRENT_BINARY_DIR}/misc/cmake/${TARGET_NAME}-config-version.cmake
-        VERSION ${PFL_ADD_LIBRARY_VERSION}
-        COMPATIBILITY SameMajorVersion)
-
-      install(
-        FILES
-          ${CMAKE_CURRENT_BINARY_DIR}/misc/cmake/${TARGET_NAME}-config.cmake
-          ${CMAKE_CURRENT_BINARY_DIR}/misc/cmake/${TARGET_NAME}-config-version.cmake
-        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME})
+    string(TOLOWER "${EXPORT_NAME}" LOWER_EXPORT_NAME)
+    set(CONFIG_FILE misc/cmake/${LOWER_EXPORT_NAME}-config.cmake)
+    set(VERSION_FILE misc/cmake/${LOWER_EXPORT_NAME}-config-version.cmake)
+    if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${CONFIG_FILE}.in)
+      set(CONFIG_FILE misc/cmake/${EXPORT_NAME}Config.cmake)
+      set(VERSION_FILE misc/cmake/${LOWER_EXPORT_NAME}ConfigVersion.cmake)
     endif()
+
+    if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${CONFIG_FILE}.in)
+      message(
+        FATAL_ERROR
+          "PFL: cmake config file not found."
+          "${CMAKE_CURRENT_SOURCE_DIR}/misc/cmake/${LOWER_EXPORT_NAME}-config.cmake.in"
+          "${CMAKE_CURRENT_SOURCE_DIR}/misc/cmake/${EXPORT_NAME}Config.cmake.in"
+      )
+    endif()
+
+    include(CMakePackageConfigHelpers)
+    # This will be used to replace @PACKAGE_cmakeModulesDir@
+    set(cmakeModulesDir cmake)
+
+    configure_package_config_file(
+      ${CMAKE_CURRENT_SOURCE_DIR}/${CONFIG_FILE}.in
+      ${CMAKE_CURRENT_BINARY_DIR}/${CONFIG_FILE}
+      INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}
+      PATH_VARS cmakeModulesDir
+      NO_SET_AND_CHECK_MACRO NO_CHECK_REQUIRED_COMPONENTS_MACRO)
+
+    write_basic_package_version_file(
+      ${VERSION_FILE}
+      VERSION ${PFL_ADD_LIBRARIES_VERSION}
+      COMPATIBILITY SameMajorVersion)
+
+    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${CONFIG_FILE}
+                  ${CMAKE_CURRENT_BINARY_DIR}/${VERSION_FILE}
+            DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME})
   endif()
 
   if(PFL_PREFIX)
@@ -289,40 +320,48 @@ function(pfl_add_library)
     set(PFL_PREFIX "${TARGET_DIR_NAME}")
   endif()
 
-  if(PFL_ENABLE_TESTING AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/tests")
-    message(
-      STATUS
-        "PFL:${PFL_MESSAGE_INDENT} Adding tests for library ${TARGET_EXPORT_NAME} at ${CMAKE_CURRENT_SOURCE_DIR}/tests"
-    )
-    set(PFL_MESSAGE_INDENT "${PFL_MESSAGE_INDENT}  ")
-    add_subdirectory(tests)
-    string(SUBSTRING "${PFL_MESSAGE_INDENT}" 2 -1 PFL_MESSAGE_INDENT)
+  if(PFL_ENABLE_TESTING)
+    if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/tests")
+      message(
+        WARNING
+          "PFL: Tests of library ${TARGET_ALIAS} is enabled but directory ${CMAKE_CURRENT_SOURCE_DIR}/tests not found."
+      )
+    else()
+      message(
+        STATUS
+          "PFL: Adding tests for library ${TARGET_ALIAS} at ${CMAKE_CURRENT_SOURCE_DIR}/tests"
+      )
+      add_subdirectory(tests)
+    endif()
   endif()
 
-  if(PFL_ADD_LIBRARY_EXAMPLES
-     AND PFL_BUILD_EXAMPLES
-     AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/examples")
-    message(
-      STATUS
-        "PFL:${PFL_MESSAGE_INDENT} Adding examples for library ${TARGET_EXPORT_NAME} at ${CMAKE_CURRENT_SOURCE_DIR}/examples"
-    )
-    set(PFL_MESSAGE_INDENT "${PFL_MESSAGE_INDENT}  ")
-    foreach(EXAMPLE ${PFL_ADD_LIBRARY_EXAMPLES})
-      add_subdirectory("examples/${EXAMPLE}")
-    endforeach()
-    string(SUBSTRING "${PFL_MESSAGE_INDENT}" 2 -1 PFL_MESSAGE_INDENT)
+  if(PFL_ADD_LIBRARY_EXAMPLES)
+    if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/examples")
+      message(
+        WARNING
+          "PFL: Examples of library ${TARGET_ALIAS} is enabled but directory ${CMAKE_CURRENT_SOURCE_DIR}/examples not found."
+      )
+    else()
+      if(PFL_BUILD_EXAMPLES)
+        message(
+          STATUS
+            "PFL: Adding examples for library ${TARGET_ALIAS} at ${CMAKE_CURRENT_SOURCE_DIR}/examples"
+        )
+        foreach(EXAMPLE ${PFL_ADD_LIBRARY_EXAMPLES})
+          add_subdirectory("examples/${EXAMPLE}")
+        endforeach()
+      endif()
+    endif()
   endif()
 
   if(PFL_ADD_LIBRARY_APPS AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/apps")
     message(
       STATUS
-        "PFL:${PFL_MESSAGE_INDENT} Adding apps for library ${TARGET_EXPORT_NAME} at ${CMAKE_CURRENT_SOURCE_DIR}/apps"
+        "PFL: Adding apps for library ${TARGET_ALIAS} at ${CMAKE_CURRENT_SOURCE_DIR}/apps"
     )
-    set(PFL_MESSAGE_INDENT "${PFL_MESSAGE_INDENT}  ")
     foreach(APP ${PFL_ADD_LIBRARY_APPS})
       add_subdirectory("apps/${APP}")
     endforeach()
-    string(SUBSTRING "${PFL_MESSAGE_INDENT}" 2 -1 PFL_MESSAGE_INDENT)
   endif()
 endfunction()
 
@@ -351,15 +390,22 @@ function(pfl_add_executable)
 
   string(REPLACE "::" "__" TARGET_PREFIX "${PFL_PREFIX}")
 
-  if(TARGET_PREFIX)
-    set(TARGET_NAME "${TARGET_PREFIX}__${TARGET_NAME}")
+  set(TARGET_NAME "${TARGET_PREFIX}__${TARGET_NAME}")
+
+  string(REPLACE "__" "::" TARGET_ALIAS "${TARGET_NAME}")
+
+  if(NOT PFL_ADD_EXECUTABLE_OUTPUT_NAME)
+    set(PFL_ADD_EXECUTABLE_OUTPUT_NAME "${TARGET_NAME}")
+    message(
+      WARNING
+        "PFL: OUTPUT_NAME of ${TARGET_ALIAS} not set, using ${PFL_ADD_EXECUTABLE_OUTPUT_NAME}"
+    )
   endif()
 
   message(
     STATUS
-      "PFL:${PFL_MESSAGE_INDENT} Adding executable ${TARGET_NAME} at ${CMAKE_CURRENT_SOURCE_DIR}"
+      "PFL: Adding executable ${PFL_ADD_EXECUTABLE_OUTPUT_NAME} as ${TARGET_ALIAS} at ${CMAKE_CURRENT_SOURCE_DIR}"
   )
-  set(PFL_MESSAGE_INDENT "${PFL_MESSAGE_INDENT}  ")
 
   __pfl_configure_files(
     INS ${PFL_ADD_EXECUTABLE_INS} HEADERS PFL_ADD_EXECUTABLE_HEADERS SOURCES
@@ -367,10 +413,12 @@ function(pfl_add_executable)
 
   add_executable("${TARGET_NAME}" ${PFL_ADD_EXECUTABLE_SOURCES})
 
-  if(PFL_ADD_EXECUTABLE_OUTPUT_NAME)
-    set_target_properties(
-      "${TARGET_NAME}" PROPERTIES OUTPUT_NAME ${PFL_ADD_EXECUTABLE_OUTPUT_NAME})
+  if(NOT "${TARGET_ALIAS}" STREQUAL "${TARGET_NAME}")
+    add_executable("${TARGET_ALIAS}" ALIAS "${TARGET_NAME}")
   endif()
+
+  set_target_properties(
+    "${TARGET_NAME}" PROPERTIES OUTPUT_NAME ${PFL_ADD_EXECUTABLE_OUTPUT_NAME})
 
   target_sources(
     ${TARGET_NAME}
@@ -413,3 +461,5 @@ function(pfl_add_executable)
     DESTINATION ${CMAKE_INSTALL_BINDIR}/${PFL_ADD_EXECUTABLE_INSTALL_PREFIX})
 
 endfunction()
+
+set_property(GLOBAL PROPERTY PFL_INITIALIZED true)

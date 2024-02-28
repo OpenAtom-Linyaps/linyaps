4ae65f77ee3d24e7e15f6fef37867bd04e559155
myml <wurongjie@deepin.org>
2024-02-28 18:05:05 +0800

    feat: Container failure should return an error
    容器运行失败时应该返回错误, 否则无法判断构建是否成功
    
    Log:

--- src/linglong/builder/linglong_builder.h
+++ src/linglong/builder/linglong_builder.h
@@ -69,7 +69,7 @@ private:
                                                            const QString &upperdir,
                                                            const QString &workdir);
 
-    static linglong::utils::error::Result<int>
+    static linglong::utils::error::Result<void>
     startContainer(QString workdir, ocppi::cli::CLI &cli, ocppi::runtime::config::types::Config &r);
     static auto toJSON(const ocppi::runtime::config::types::Mount &) -> nlohmann::json;
     static auto toJSON(const ocppi::runtime::config::types::Config &) -> nlohmann::json;

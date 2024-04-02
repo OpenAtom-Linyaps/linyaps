5538b717a6c574bf402ca9ed3aeecff46e14ee60
black-desk <me@black-desk.cn>
2024-04-02 20:21:22 +0800

    fix: correct bundle directory
    Signed-off-by: black-desk <me@black-desk.cn>

--- src/linglong/runtime/container.cpp
+++ src/linglong/runtime/container.cpp
@@ -32,32 +32,33 @@ Container::run(const ocppi::runtime::config::types::Process &process) noexcept
     LINGLONG_TRACE(QString("run container %1").arg(this->id.toString()));
 
     QDir runtimeDir = QStandardPaths::writableLocation(QStandardPaths::RuntimeLocation);
-    if (!runtimeDir.mkpath(QString("linglong/%1").arg(this->id.toString()))) {
+    QDir bundle = runtimeDir.absoluteFilePath(QString("linglong/%1").arg(this->id.toString()));
+    Q_ASSERT(bundle.exists());
+    if (!bundle.mkpath(".")) {
         return LINGLONG_ERR("make bundle directory");
     }
 
     auto _ = utils::finally::finally([&]() {
-        if (runtimeDir.removeRecursively()) {
+        if (bundle.removeRecursively()) {
             return;
         }
 
         qCritical() << "failed to remove" << runtimeDir.absolutePath();
     });
 
-    QDir bundle = runtimeDir.absoluteFilePath(QString("linglong/%1").arg(this->id.toString()));
-    Q_ASSERT(bundle.exists());
-
     this->cfg.process = process;
 
     nlohmann::json json = this->cfg;
 
-    std::ofstream ofs(bundle.absoluteFilePath("config.json").toStdString());
-    Q_ASSERT(ofs.is_open());
-    if (!ofs.is_open()) {
-        return LINGLONG_ERR("create config.json in bundle directory");
-    }
+    {
+        std::ofstream ofs(bundle.absoluteFilePath("config.json").toStdString());
+        Q_ASSERT(ofs.is_open());
+        if (!ofs.is_open()) {
+            return LINGLONG_ERR("create config.json in bundle directory");
+        }
 
-    ofs << json.dump(4);
+        ofs << json.dump();
+    }
 
     auto result = this->cli.run(ocppi::runtime::ContainerID(this->id.toString().toStdString()),
                                 std::filesystem::path(bundle.absolutePath().toStdString()));

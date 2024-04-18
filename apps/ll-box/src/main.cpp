1262f0ec98889d99802efca4f4a96f666b6b7f3f
black-desk <me@black-desk.cn>
2024-04-18 15:33:57 +0800

    refact(ll-box): use argp
    Signed-off-by: black-desk <me@black-desk.cn>

--- apps/ll-box/src/main.cpp
+++ apps/ll-box/src/main.cpp
@@ -9,6 +9,8 @@
 #include "util/message_reader.h"
 #include "util/oci_runtime.h"
 
+#include <argp.h>
+
 #include <iostream>
 
 #include <fcntl.h>
@@ -16,18 +18,243 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc, char **argv)
+namespace {
+
+constexpr auto cgroup_manager_option = 1111;
+constexpr auto bundle_option = 2222;
+constexpr auto config_option = 3333;
+
+std::optional<std::string> command;
+std::optional<std::string> container;
+std::optional<std::string> signal;
+
+std::string bundle = ".";
+std::string config = "config.json";
+
+std::optional<std::vector<std::string>> commands;
+
+int parse_opt(int key, char *arg, struct argp_state *state)
 {
-    try {
-        auto config = std::ifstream(argv[1]);
+    switch (key) {
+    case cgroup_manager_option:
+        if (command) {
+            argp_failure(state, 1, 0, "global option must be specified before the command");
+            return -1;
+        }
 
-        auto json = nlohmann::json::parse(config);
-        auto runtime = json.get<linglong::Runtime>();
+        if (strcmp(arg, "disabled") == 0) {
+            return 0;
+        }
 
-        linglong::Container container(runtime);
-        return container.Start();
-    } catch (const std::exception &e) {
-        logErr() << "failed: " << e.what();
+        argp_failure(state, 1, 0, "invalid cgroup manager", arg);
+        return -1;
+
+    case bundle_option:
+        if (!command) {
+            argp_failure(state, 1, 0, "subcommand option must be specified after the command");
+            return -1;
+        }
+
+        if (command != "run") {
+            argp_failure(state, 1, 0, "unknown option for command %s: bundle", command->c_str());
+            return -1;
+        }
+
+        bundle = arg;
+        return 0;
+
+    case config_option:
+        if (!command) {
+            argp_failure(state, 1, 0, "subcommand option must be specified after the command");
+            return -1;
+        }
+
+        if (command != "run") {
+            argp_failure(state, 1, 0, "unknown option for command %s: config", command->c_str());
+            return -1;
+        }
+
+        config = arg;
+        return 0;
+
+    case ARGP_KEY_ARG:
+        if (!command) {
+            command = arg;
+            return 0;
+        }
+
+        if (command == "run") {
+            if (!container) {
+                container = arg;
+                return 0;
+            }
+
+            argp_failure(state, 1, 0, "command %s takes only one argument", command->c_str());
+            return -1;
+        }
+
+        if (command == "exec") {
+            if (!container) {
+                container = arg;
+                return 0;
+            }
+
+            if (!commands) {
+                commands = std::vector<std::string>{ arg };
+                return 0;
+            }
+
+            commands->push_back(arg);
+            return 0;
+        }
+
+        if (command == "kill") {
+            if (!container) {
+                container = arg;
+                return 0;
+            }
+
+            if (!signal) {
+                signal = arg;
+                return 0;
+            }
+
+            argp_failure(state, 1, 0, "command %s takes only two argument", command->c_str());
+            return -1;
+        }
+
+        return -1;
+
+    case ARGP_KEY_END:
+        if (!command) {
+            argp_failure(state, 1, 0, "command is required");
+            return -1;
+        }
+
+        if (command != "list" && !container) {
+            argp_failure(state, 1, 0, "container is required");
+            return -1;
+        }
+
+        if (command == "exec" && !commands) {
+            argp_failure(state, 1, 0, "commands is required");
+            return -1;
+        }
+
+        if (command == "kill" && !signal) {
+            argp_failure(state, 1, 0, "signal is required");
+            return -1;
+        }
+
+        return 0;
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+}
+
+int list() noexcept
+{
+    logErr() << "Not implemented yet";
+    return -1;
+}
+
+int exec() noexcept
+{
+    logErr() << "Not implemented yet";
+    return -1;
+}
+
+int run() noexcept
+try {
+    auto configFile = std::ifstream(bundle + "/" + config);
+    if (!configFile.is_open()) {
+        logErr() << "failed to open config" << bundle + "/" + config;
         return -1;
     }
+
+    auto json = nlohmann::json::parse(configFile);
+    auto runtime = json.get<linglong::Runtime>();
+
+    linglong::Container container(runtime);
+    return container.Start();
+} catch (const std::exception &e) {
+    logErr() << "run failed:" << e.what();
+    return -1;
+}
+
+} // namespace
+
+int main(int argc, char **argv)
+{
+
+    argp_option options[] = {
+        {
+          .name = nullptr,
+          .key = 0,
+          .arg = nullptr,
+          .flags = 0,
+          .doc = "Global options",
+          .group = 1,
+        },
+        {
+          .name = "cgroup-manager",
+          .key = cgroup_manager_option,
+          .arg = "MODE",
+          .flags = 0,
+          .doc = "allowed values: disabled",
+          .group = 1,
+        },
+        {
+          .name = nullptr,
+          .key = 0,
+          .arg = nullptr,
+          .flags = 0,
+          .doc = "Run options",
+          .group = 2,
+        },
+        {
+          .name = "bundle",
+          .key = bundle_option,
+          .arg = "DIR",
+          .flags = 0,
+          .doc = "Path to the root of the bundle dir (default \".\")",
+          .group = 2,
+        },
+        {
+          .name = "config",
+          .key = config_option,
+          .arg = "FILE",
+          .flags = 0,
+          .doc = "Override the configuration file to use. The default value is config.json",
+          .group = 2,
+        },
+        { nullptr },
+    };
+
+    argp argp = {
+        .options = options,
+        .parser = parse_opt,
+        .args_doc = R"(list
+run <CONTAINER>
+exec <CONTAINER> <CMD>
+kill <CONTAINER> <SIGNAL>)",
+    };
+
+    if (argp_parse(&argp, argc, argv, 0, nullptr, nullptr) != 0) {
+        return -1;
+    }
+
+    if (command == "list") {
+        return list();
+    }
+
+    if (command == "run") {
+        return run();
+    }
+
+    if (command == "exec") {
+        return exec();
+    }
+
+    return -1;
 }

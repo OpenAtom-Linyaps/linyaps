f2ebc5bf0ac8f7932897cb20087394d7ad344fd4
dengbo <dengbo@deepin.org>
2024-05-28 14:08:13 +0800

    Revert "fix: timezone is error for linglong app"
    This reverts commit 69efe374c5aa8016eb0463706c9cbe0315e7f9b4.

--- apps/generators/90-legacy/src/main.cpp
+++ apps/generators/90-legacy/src/main.cpp
@@ -45,7 +45,6 @@ int main()
         { "/etc/resolv.conf", "/etc/resolv.conf" },
         { "/etc/resolvconf", "/etc/resolvconf" },
         { "/etc/localtime", "/etc/localtime" },
-        { "/etc/timezone ", "/etc/timezone" },
     };
 
     for (const auto &[source, destination] : roMountMap) {

53ac3bf10c07637ff1d6679cb843d22fba74191a
myml <wurongjie@deepin.org>
2024-08-06 17:48:54 +0800

    fix: regex matching issue in the install list file
    修复因字符串替换导致正则匹配错误
    
    Log:

--- libs/linglong/src/linglong/builder/linglong_builder.cpp
+++ libs/linglong/src/linglong/builder/linglong_builder.cpp
@@ -261,14 +261,18 @@ utils::error::Result<void> splitDevelop(QString installFilepath,
         // 计算进度
         ruleIndex++;
         auto percentage = ruleIndex * 100 / installRules.length(); // NOLINT
-        // /opt/apps/${appid} to $PROJECT_ROOT/.../files
-        // /runtime/ to $PROJECT_ROOT/
-        rule.replace(0, prefix.length(), src);
+        // 统计进度
         if (handleProgress) {
             handleProgress(percentage);
         }
+        // 跳过注释
+        if (rule.startsWith("#")) {
+            continue;
+        }
         // 如果不以^符号开头，当作普通路径使用
         if (!rule.startsWith("^")) {
+            // replace $prefix with $PROJECT_ROOT/output/$model/files
+            rule.replace(0, prefix.length(), src);
             QFileInfo info(rule);
             // 链接指向的文件如果不存在，info.exists会返回false
             // 所以要先判断文件是否是链接
@@ -283,6 +287,10 @@ utils::error::Result<void> splitDevelop(QString installFilepath,
             }
             continue;
         }
+        // replace $prefix with $PROJECT_ROOT/output/$model/files
+        // TODO(wurongjie) 应该只替换一次, 避免路径包含多个prefix
+        // 但不能使用 rule.replace(0, prefix.length), 会导致正则匹配错误
+        rule.replace(prefix, src);
         // convert prefix in container to real path in host
         QRegularExpression regexp(rule);
         // reverse files in src
@@ -808,7 +816,7 @@ set -e
     printReplacedText(QString("%1%2%3%4")
                         .arg(info.id.c_str(), -25)      // NOLINT
                         .arg(info.version.c_str(), -15) // NOLINT
-                        .arg("binary", -15)             // NOLINT
+                        .arg("develop", -15)            // NOLINT
                         .arg("complete\n")
                         .toStdString(),
                       2);

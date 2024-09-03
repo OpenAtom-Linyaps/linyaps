df449dc70434f544b539f2d52738b3efbc03cbcc
kamiyadm <chenhuixing@deepin.org>
2024-09-03 17:00:35 +0800

    feat: overload loadJSONFile with other type
    Support std::filesystem and GFile.
    
    Log:

--- libs/utils/src/linglong/utils/packageinfo_handler.h
+++ libs/utils/src/linglong/utils/packageinfo_handler.h
@@ -8,6 +8,7 @@
 #include "linglong/api/types/v1/PackageInfoV2.hpp"
 #include "linglong/utils/error/error.h"
 
+#include <gio/gio.h>
 #include <QString>
 
 #define PACKAGE_INFO_VERSION "1.0"
@@ -16,4 +17,5 @@ namespace linglong::utils {
 error::Result<api::types::v1::PackageInfoV2> toPackageInfoV2(const api::types::v1::PackageInfo &oldInfo);
 error::Result<api::types::v1::PackageInfoV2> parsePackageInfo(const QString &path);
 error::Result<api::types::v1::PackageInfoV2> parsePackageInfo(const nlohmann::json &json);
+error::Result<api::types::v1::PackageInfoV2> parsePackageInfo(GFile *file);
 }; // namespace linglong::utils

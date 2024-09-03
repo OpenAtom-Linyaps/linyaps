287606321f9d769e202b5660551e85851afd7b7b
ComixHe <heyuming@deepin.org>
2024-09-03 15:19:31 +0800

    refactor: change repoCache
    Signed-off-by: ComixHe <heyuming@deepin.org>

--- libs/linglong/src/linglong/repo/repo_cache.cpp
+++ libs/linglong/src/linglong/repo/repo_cache.cpp
@@ -101,56 +101,137 @@ scanAllLayers(const OstreeRepo &repo)
 {
 }
 
-utils::error::Result<std::map<std::string, api::types::v1::RepositoryCacheLayersItem>::iterator>
-RepoCache::addLayerItem(const std::string &ref,
-                        const api::types::v1::RepositoryCacheLayersItem &item)
+utils::error::Result<void>
+RepoCache::addLayerItem(const cacheRef &ref, const api::types::v1::RepositoryCacheLayersItem &item)
 {
     LINGLONG_TRACE("add layer item");
-    auto it = this->cache.layers.insert(std::make_pair(ref, item));
-    if (!it.second) {
-        return LINGLONG_ERR("already exists");
+
+    if (!ref.version || !ref.repo || !ref.channel || !ref.module || ref.id.empty()) {
+        return LINGLONG_ERR("unqualified cache ref");
     }
 
-    return it.first;
-}
+    std::string cacheRef = ref.repo.value() + ":" + ref.channel.value() + "/" + ref.id + "/"
+      + ref.version.value() + linglong::repo::cacheRef::arch + ref.module.value();
 
-utils::error::Result<void> RepoCache::deleteLayerItem(const std::string &ref)
-{
-    LINGLONG_TRACE("delete layer item");
-
-    auto it = this->cache.layers.erase(ref);
-    if (it <= 0) {
-        return LINGLONG_ERR("not found");
+    if (ref.uuid) {
+        cacheRef += "_" + ref.uuid.value();
     }
 
+    cache.layers.emplace(std::move(cacheRef), item);
     return LINGLONG_OK;
 }
 
-utils::error::Result<std::map<std::string, api::types::v1::RepositoryCacheLayersItem>::iterator>
-RepoCache::updateLayerItem(const std::string &ref,
-                           const api::types::v1::RepositoryCacheLayersItem &item)
+utils::error::Result<void> RepoCache::deleteLayerItem(const cacheRef &ref)
 {
-    LINGLONG_TRACE("update layer item");
-    auto it = this->cache.layers.find(ref);
-    if (it != this->cache.layers.end()) {
-        it->second = item;
-        return it;
+    LINGLONG_TRACE("delete layer item");
+
+    if (!ref.version || !ref.repo || !ref.channel || !ref.module || ref.id.empty()) {
+        return LINGLONG_ERR("unqualified cache ref");
     }
 
-    return LINGLONG_ERR("not found");
+    std::string cacheRef = ref.repo.value() + ":" + ref.channel.value() + "/" + ref.id + "/"
+      + ref.version.value() + linglong::repo::cacheRef::arch + ref.module.value();
+
+    if (ref.uuid) {
+        cacheRef += "_" + ref.uuid.value();
+    }
+
+    cache.layers.erase(cacheRef);
+    return LINGLONG_OK;
 }
 
-utils::error::Result<api::types::v1::RepositoryCacheLayersItem>
-RepoCache::searchLayerItem(const std::string &ref)
+utils::error::Result<std::vector<package::Reference>>
+RepoCache::searchLayerItem(const cacheRef &ref)
 {
     LINGLONG_TRACE("search layer item");
 
-    auto it = this->cache.layers.find(ref);
-    if (it != this->cache.layers.end()) {
-        return it->second;
+    std::vector<package::Reference> refs;
+    for (const auto &[ref, item] : cache.layers) {
+        // format: repo:channel/id/version/arch/module[_uuid]
+        auto location = ref.find(':');
+        if (location == std::string::npos) {
+            qCritical() << "invalid content of cache ref:" << QString::fromStdString(ref);
+            continue;
+        }
+
+        std::string channel;
+        std::string id;
+        std::string version;
+
+        auto newRef = ref.substr(location + 1); // no need to match 'repo' for now
+        std::istringstream stream{ newRef };
+        std::string component;
+        while (std::getline(stream, component, '/')) {
+            if (channel.empty()) {
+                channel = std::move(component);
+                continue;
+            }
+
+            if (id.empty()) {
+                id = std::move(component);
+                continue;
+            }
+
+            if (version.empty()) {
+                version = std::move(component);
+                continue;
+            }
+
+            break;
+        }
+
+        auto packageVer = package::Version::parse(QString::fromStdString(version));
+        if (!packageVer) {
+            qCritical() << packageVer.error() << ":" << QString::fromStdString(ref);
+            continue;
+        }
+
+        auto arch = package::Architecture::currentCPUArchitecture();
+        if (!arch) {
+            qCritical() << arch.error() << ":" << QString::fromStdString(ref);
+            continue;
+        }
+
+        auto packageRef = package::Reference::create(QString::fromStdString(channel),
+                                                     QString::fromStdString(id),
+                                                     *packageVer,
+                                                     *arch);
+        if (!packageRef) {
+            qCritical() << packageRef.error() << QString::fromStdString(ref);
+            continue;
+        }
+
+        refs.emplace_back(std::move(packageRef).value());
     }
 
-    return LINGLONG_ERR("not found");
+    auto excludeFilter = [&refs](auto pred) {
+        static_assert(std::is_invocable_r_v<bool, decltype(pred), const package::Reference &>);
+        refs.erase(std::remove_if(refs.begin(), refs.end(), pred), std::end(refs));
+    };
+
+    excludeFilter([&ref](const package::Reference &pkgRef) {
+        return ref.id != pkgRef.id.toStdString();
+    });
+
+    if (ref.channel) {
+        excludeFilter([&ref](const package::Reference &pkgRef) {
+            return ref.channel != pkgRef.channel.toStdString();
+        });
+    }
+
+    if (ref.version) {
+        excludeFilter([&ref](const package::Reference &pkgRef) {
+            return ref.channel != pkgRef.channel.toStdString();
+        });
+    }
+
+    std::sort(refs.begin(),
+              refs.end(),
+              [](const package::Reference &lhs, const package::Reference &rhs) {
+                  return lhs.version > rhs.version;
+              });
+
+    return refs;
 }
 
 utils::error::Result<void> RepoCache::writeToDisk()

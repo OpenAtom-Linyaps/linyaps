98d02dae1bde799340218cf1a325411939d8f505
ComixHe <heyuming@deepin.org>
2024-09-11 17:42:57 +0800

    refactor: redesign some methods of repoCache
    Signed-off-by: ComixHe <heyuming@deepin.org>

--- libs/linglong/src/linglong/repo/repo_cache.cpp
+++ libs/linglong/src/linglong/repo/repo_cache.cpp
@@ -15,43 +15,29 @@
 
 namespace linglong::repo {
 
-RepoCache::RepoCache(RepoCache &&other) noexcept
-    : cache(std::move(other).cache)
-    , configPath(std::move(other).configPath)
-{
-}
-
-RepoCache &RepoCache::operator=(RepoCache &&other) noexcept
-{
-    if (&other == this) {
-        return *this;
-    }
-
-    this->cache = std::move(other).cache;
-    this->configPath = std::move(other).configPath;
-    return *this;
-}
-
 utils::error::Result<std::unique_ptr<RepoCache>>
-RepoCache::create(const std::filesystem::path &repoRoot,
+RepoCache::create(const std::filesystem::path &cacheFile,
                   const api::types::v1::RepoConfig &repoConfig,
                   OstreeRepo &repo)
 {
-    LINGLONG_TRACE("create RepoCache");
+    LINGLONG_TRACE("load from RepoCache");
 
     struct enableMaker : public RepoCache
     {
         using RepoCache::RepoCache;
     };
 
+    // making the constructor of RepoCache be public within this function
+    // see also: https://seanmiddleditch.github.io/enabling-make-unique-with-private-constructors
     auto repoCache = std::make_unique<enableMaker>();
-    repoCache->configPath = repoRoot / "cache.json";
+    repoCache->cacheFile = cacheFile;
     std::error_code ec;
-    if (!std::filesystem::exists(repoCache->configPath, ec)) {
+    if (!std::filesystem::exists(repoCache->cacheFile, ec)) {
         if (ec) {
             std::string error = "checking file existence failed: " + ec.message();
             return LINGLONG_ERR(error.c_str());
         }
+
         auto ret = repoCache->rebuildCache(repoConfig, repo);
         if (!ret) {
             return LINGLONG_ERR(ret);
@@ -60,7 +46,7 @@ RepoCache::create(const std::filesystem::path &repoRoot,
     }
 
     auto result = utils::serialize::LoadJSONFile<api::types::v1::RepositoryCache>(
-      QString::fromStdString(repoCache->configPath));
+      QString::fromStdString(repoCache->cacheFile.string()));
     if (!result) {
         std::cout << "invalid cache file, rebuild cache..." << std::endl;
         auto ret = repoCache->rebuildCache(repoConfig, repo);
@@ -71,18 +57,8 @@ RepoCache::create(const std::filesystem::path &repoRoot,
     }
 
     repoCache->cache = std::move(result).value();
-    std::string cliVersion = LINGLONG_VERSION;
-    if (result->llVersion > cliVersion) {
-        std::cout << "linglong cli version is older than cache file, rebuild cache..." << std::endl;
-        auto ret = repoCache->rebuildCache(repoConfig, repo);
-        if (!ret) {
-            return LINGLONG_ERR(ret);
-        }
-        return repoCache;
-    }
-
-    if (repoCache->isLayerEmpty()) {
-        std::cout << "layer in cache is empty, try to rebuild cache..." << std::endl;
+    if (result->version != "1" || result->llVersion != LINGLONG_VERSION) {
+        std::cout << "The existing cache is outdated, rebuild cache..." << std::endl;
         auto ret = repoCache->rebuildCache(repoConfig, repo);
         if (!ret) {
             return LINGLONG_ERR(ret);
@@ -95,66 +71,61 @@ RepoCache::create(const std::filesystem::path &repoRoot,
     return repoCache;
 }
 
-bool RepoCache::isLayerEmpty() const noexcept
-{
-    return this->cache.layers.empty();
-}
-
 utils::error::Result<void> RepoCache::rebuildCache(const api::types::v1::RepoConfig &repoConfig,
-                                                   OstreeRepo &repo)
+                                                   OstreeRepo &repo) noexcept
 {
     LINGLONG_TRACE("rebuild repo cache");
 
     this->cache.llVersion = LINGLONG_VERSION;
     this->cache.config = repoConfig;
-    this->cache.version = "0.0.1";
+    this->cache.version = "1";
 
     g_autoptr(GHashTable) refsTable = nullptr;
     g_autoptr(GError) gErr = nullptr;
-    std::vector<std::string> refs;
+    std::vector<std::string_view> refs;
 
-    if (!ostree_repo_list_refs(&repo, nullptr, &refsTable, nullptr, &gErr)) {
+    if (ostree_repo_list_refs(&repo, nullptr, &refsTable, nullptr, &gErr) == FALSE) {
         return LINGLONG_ERR("ostree_repo_list_refs", gErr);
     }
 
+    // we couldn't report error within below lambda and for_each wouldn't return early if error
+    // occurred, copy refs out
     g_hash_table_foreach(
       refsTable,
       [](gpointer key, gpointer value, gpointer data) {
           // key,value -> ref,checksum
-          std::vector<std::string> *vec = static_cast<std::vector<std::string> *>(data);
-          vec->emplace_back(static_cast<char *>(key));
+          auto *vec = static_cast<std::vector<std::string_view> *>(data);
+          vec->emplace_back(static_cast<const char *>(key));
       },
       &refs);
 
-    for (const auto ref : refs) {
-        g_autofree char *commit = nullptr;
-        g_autoptr(GError) gErr = nullptr;
-        g_autoptr(GFile) root = nullptr;
-        api::types::v1::RepositoryCacheLayersItem item;
+    for (auto ref : refs) {
+        auto pos = ref.find(':');
+        if (pos == std::string::npos) {
+            std::cout << "invalid ref: " << ref << std::endl;
+            continue;
+        }
 
-        g_clear_error(&gErr);
-        if (!ostree_repo_read_commit(&repo, ref.c_str(), &root, &commit, nullptr, &gErr)) {
+        api::types::v1::RepositoryCacheLayersItem item;
+        item.repo = ref.substr(0, pos);
+
+        g_autofree char *commit{ nullptr };
+        g_autoptr(GError) gErr{ nullptr };
+        g_autoptr(GFile) root{ nullptr };
+        if (ostree_repo_read_commit(&repo, ref.data(), &root, &commit, nullptr, &gErr) == FALSE) {
             return LINGLONG_ERR("ostree_repo_read_commit", gErr);
         }
+        item.commit = commit;
 
         // ostree ls --repo repo ref, the file path of info.json is /info.json.
         g_autoptr(GFile) infoFile = g_file_resolve_relative_path(root, "info.json");
-
         auto info = utils::parsePackageInfo(infoFile);
         if (!info) {
             return LINGLONG_ERR(info);
         }
-
-        item.commit = commit;
         item.info = *info;
-        size_t pos = ref.find(":");
-        if (pos != std::string::npos) {
-            item.repo = ref.substr(0, pos);
-        } else {
-            std::cout << "invalid ref: " << ref;
-            continue;
-        }
-        this->cache.layers.emplace_back(item);
+
+        this->cache.layers.emplace_back(std::move(item));
     }
 
     auto ret = writeToDisk();
@@ -176,11 +147,13 @@ RepoCache::addLayerItem(const api::types::v1::RepositoryCacheLayersItem &item)
       [&item](const api::types::v1::RepositoryCacheLayersItem &val) {
           return !(item.commit != val.commit || item.repo != val.repo
                    || item.info.channel != val.info.channel || item.info.id != val.info.id
-                   || item.info.version != val.info.version || item.info.arch != val.info.arch
+                   || item.info.version != val.info.version
+                   || item.info.arch.front() != val.info.arch.front()
                    || item.info.packageInfoV2Module != val.info.packageInfoV2Module);
       });
 
     if (it != cache.layers.end()) {
+        assert(false);
         return LINGLONG_ERR("item already exist");
     }
 
@@ -194,7 +167,7 @@ RepoCache::addLayerItem(const api::types::v1::RepositoryCacheLayersItem &item)
 }
 
 utils::error::Result<void>
-RepoCache::deleteLayerItem(const api::types::v1::RepositoryCacheLayersItem &item)
+RepoCache::deleteLayerItem(const api::types::v1::RepositoryCacheLayersItem &item) noexcept
 {
     LINGLONG_TRACE("delete layer item");
 
@@ -204,11 +177,13 @@ RepoCache::deleteLayerItem(const api::types::v1::RepositoryCacheLayersItem &item
       [&item](const api::types::v1::RepositoryCacheLayersItem &val) {
           return !(item.commit != val.commit || item.repo != val.repo
                    || item.info.channel != val.info.channel || item.info.id != val.info.id
-                   || item.info.version != val.info.version || item.info.arch != val.info.arch
+                   || item.info.version != val.info.version
+                   || item.info.arch.front() != val.info.arch.front()
                    || item.info.packageInfoV2Module != val.info.packageInfoV2Module);
       });
 
     if (it == cache.layers.end()) {
+        assert(false);
         return LINGLONG_ERR("item doesn't exist");
     }
 
@@ -222,62 +197,56 @@ RepoCache::deleteLayerItem(const api::types::v1::RepositoryCacheLayersItem &item
 }
 
 std::vector<api::types::v1::RepositoryCacheLayersItem>
-RepoCache::searchLayerItem(const CacheRef &ref) const
+RepoCache::queryLayerItem(const repoCacheQuery &query) const noexcept
 {
     using itemRef = std::reference_wrapper<const api::types::v1::RepositoryCacheLayersItem>;
-    std::vector<itemRef> layers;
+    std::vector<itemRef> layers_view;
     for (const auto &layer : cache.layers) {
-        // if id is empty, return all items.
-        if (ref.id.empty()) {
-            layers.emplace_back(layer);
+        if (query.id && query.id.value() != layer.info.id) {
             continue;
         }
 
-        if (ref.id != layer.info.id) {
+        if (query.repo && query.repo.value() != layer.repo) {
             continue;
         }
 
-        if (ref.repo && ref.repo.value() != layer.repo) {
+        if (query.channel && query.channel.value() != layer.info.channel) {
             continue;
         }
 
-        if (ref.channel && ref.channel.value() != layer.info.channel) {
+        if (query.version && query.version.value() != layer.info.version) {
             continue;
         }
 
-        if (ref.version && ref.version.value() != layer.info.version) {
+        if (query.module && query.module.value() != layer.info.packageInfoV2Module) {
             continue;
         }
 
-        if (ref.module && ref.module.value() != layer.info.packageInfoV2Module) {
-            continue;
-        }
-
-        if (ref.uuid) {
+        if (query.uuid) {
             if (!layer.info.uuid) {
                 continue;
             }
 
-            if (ref.uuid.value() != layer.info.uuid.value()) {
+            if (query.uuid.value() != layer.info.uuid.value()) {
                 continue;
             }
         }
 
-        layers.emplace_back(layer);
+        layers_view.emplace_back(layer);
     }
 
-    std::sort(layers.begin(), layers.end(), [](itemRef lhs, itemRef rhs) {
+    std::sort(layers_view.begin(), layers_view.end(), [](itemRef lhs, itemRef rhs) {
         return lhs.get().info.version > rhs.get().info.version;
     });
 
-    return std::vector<api::types::v1::RepositoryCacheLayersItem>(layers.cbegin(), layers.cend());
+    return { layers_view.cbegin(), layers_view.cend() };
 }
 
 utils::error::Result<void> RepoCache::writeToDisk()
 {
     LINGLONG_TRACE("save repo cache");
 
-    auto ofs = std::ofstream(this->configPath, std::ofstream::out | std::ofstream::trunc);
+    auto ofs = std::ofstream(this->cacheFile, std::ofstream::out | std::ofstream::trunc);
     if (!ofs.is_open()) {
         return LINGLONG_ERR("open failed");
     }

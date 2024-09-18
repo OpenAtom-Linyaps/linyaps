33a76c5b67bce7812a32de4d1e9e6cf63b1d8002
ComixHe <heyuming@deepin.org>
2024-09-18 14:11:57 +0800

    refactor!: adjust dbus interface and api of package manager
    Signed-off-by: ComixHe <heyuming@deepin.org>

---
+++ libs/linglong/src/linglong/adaptors/task/task1.h
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#pragma once
+
+#include "linglong/adaptors/task/gen_org_deepin_linglong_task1.h"
+
+namespace linglong::adaptors::package_manger {
+using Task1 = OrgDeepinLinglongTask1Adaptor;
+}

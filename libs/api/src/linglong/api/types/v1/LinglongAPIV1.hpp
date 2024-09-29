aac0a08867eccfb929dc2864ec691a3a66f9a7a9
kamiyadm <chenhuixing@deepin.org>
2024-09-29 13:53:59 +0800

    refactor: Change some data structures in the schema
    * Remove PackageTaskMessage, PackageManager1ResultWithTaskID.
    * Add State, SubState, PackageManager1ResultWithTaskObjectPath.
    
    Log:

--- libs/api/src/linglong/api/types/v1/LinglongAPIV1.hpp
+++ libs/api/src/linglong/api/types/v1/LinglongAPIV1.hpp
@@ -30,17 +30,27 @@
 #include "linglong/api/types/v1/PackageInfoV2.hpp"
 #include "linglong/api/types/v1/PackageManager1GetRepoInfoResult.hpp"
 #include "linglong/api/types/v1/PackageManager1InstallParameters.hpp"
-#include "linglong/api/types/v1/PackageManager1ResultWithTaskID.hpp"
+#include "linglong/api/types/v1/PackageManager1ResultWithTaskObjectPath.hpp"
 #include "linglong/api/types/v1/PackageManager1ModifyRepoParameters.hpp"
 #include "linglong/api/types/v1/PackageManager1Package.hpp"
 #include "linglong/api/types/v1/PackageManager1SearchParameters.hpp"
 #include "linglong/api/types/v1/PackageManager1SearchResult.hpp"
 #include "linglong/api/types/v1/PackageManager1UninstallParameters.hpp"
 #include "linglong/api/types/v1/PackageManager1UpdateParameters.hpp"
-#include "linglong/api/types/v1/PackageTaskMessage.hpp"
 #include "linglong/api/types/v1/RepoConfig.hpp"
 #include "linglong/api/types/v1/RepositoryCache.hpp"
 #include "linglong/api/types/v1/UabMetaInfo.hpp"
+
+namespace linglong {
+namespace api {
+namespace types {
+namespace v1 {
+enum class State : int;
+enum class SubState : int;
+}
+}
+}
+}
 
 namespace linglong {
 namespace api {
@@ -72,7 +82,7 @@ std::optional<PackageInfoV2> packageInfoV2;
 std::optional<PackageManager1GetRepoInfoResult> packageManager1GetRepoInfoResult;
 std::optional<CommonResult> packageManager1InstallLayerFDResult;
 std::optional<PackageManager1InstallParameters> packageManager1InstallParameters;
-std::optional<PackageManager1ResultWithTaskID> packageManager1InstallResult;
+std::optional<PackageManager1ResultWithTaskObjectPath> packageManager1InstallResult;
 std::optional<PackageManager1ModifyRepoParameters> packageManager1ModifyRepoParameters;
 std::optional<CommonResult> packageManager1ModifyRepoResult;
 std::optional<PackageManager1Package> packageManager1Package;
@@ -81,10 +91,11 @@ std::optional<PackageManager1SearchResult> packageManager1SearchResult;
 std::optional<PackageManager1UninstallParameters> packageManager1UninstallParameters;
 std::optional<CommonResult> packageManager1UninstallResult;
 std::optional<PackageManager1UpdateParameters> packageManager1UpdateParameters;
-std::optional<PackageManager1ResultWithTaskID> packageManager1UpdateResult;
-std::optional<PackageTaskMessage> packageTaskMessage;
+std::optional<PackageManager1ResultWithTaskObjectPath> packageManager1UpdateResult;
 std::optional<RepoConfig> repoConfig;
 std::optional<RepositoryCache> repositoryCache;
+std::optional<State> state;
+std::optional<SubState> subState;
 std::optional<UabMetaInfo> uabMetaInfo;
 };
 }

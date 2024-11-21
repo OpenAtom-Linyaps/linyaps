OpenAtomFoundation/linglong

feat: support base custom application installation location
base可以通过app_prefix信息设置应用安装位置 如果base设置了app_prefix,
玲珑在构建时会将PREFIX设置成app_prefix指定的目录
在运行时也会将应用文件挂载成app_prefix指定的目录

apps/generators/25-host-env/src/main.cpp
@@ -6,6 +6,7 @@
 
 #include <filesystem>
 #include <iostream>
+#include <string>
 
 extern char **environ;
 
@@ -80,9 +81,11 @@ int main()
     }
 
     auto annotations = content.at("annotations");
-    env.push_back("LINGLONG_APPID="
-                  + annotations.at("org.deepin.linglong.appID").get<std::string>());
-
+    auto appID = annotations.at("org.deepin.linglong.appID").get<std::string>();
+    // appPrefix
+    auto appPrefix = annotations.at("org.deepin.linglong.appPrefix").get<std::string>();
+    env.push_back("LINGLONG_APPID=" + appID);
+    env.push_back("LINGLONG_appPrefix=" + appPrefix);
     std::cout << content.dump() << std::endl;
     return 0;
 }

OpenAtom-Linyaps/linyaps

apps/generators/25-host-env/src/main.cpp
@@ -6,6 +6,7 @@
 
 #include <filesystem>
 #include <iostream>
+#include <string>
 
 extern char **environ;
 
@@ -79,9 +80,11 @@ int main()
     }
 
     auto annotations = content.at("annotations");
-    env.push_back("LINGLONG_APPID="
-                  + annotations.at("org.deepin.linglong.appID").get<std::string>());
-
+    auto appID = annotations.at("org.deepin.linglong.appID").get<std::string>();
+    // appPrefix
+    auto appPrefix = annotations.at("org.deepin.linglong.appPrefix").get<std::string>();
+    env.push_back("LINGLONG_APPID=" + appID);
+    env.push_back("LINGLONG_APP_PREFIX=" + appPrefix);
     std::cout << content.dump() << std::endl;
     return 0;
 }
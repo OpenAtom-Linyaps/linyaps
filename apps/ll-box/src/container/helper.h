df1ee7bb8fb26a9318dda5147c7c0a20d32d424c
ComixHe <heyuming@deepin.org>
2024-12-01 01:35:19 +0800

    feat(ll-box): support global option 'root'
    refactor the way of parsing and executing command
    now parsing all args at first
    
    Signed-off-by: ComixHe <heyuming@deepin.org>

--- apps/ll-box/src/container/helper.h
+++ apps/ll-box/src/container/helper.h
@@ -7,9 +7,13 @@
 #pragma once
 
 #include <nlohmann/json.hpp>
+
 #include <string>
 
 namespace linglong {
-void writeContainerJson(const std::string &bundle, const std::string &id, pid_t pid);
-nlohmann::json readAllContainerJson() noexcept;
+void writeContainerJson(const std::filesystem::path &stateRoot,
+                        const std::string &bundle,
+                        const std::string &id,
+                        pid_t pid);
+nlohmann::json readAllContainerJson(const std::filesystem::path &stateRoot) noexcept;
 }; // namespace linglong

d67d6a87f37f47c639cc138bd85fdf22fff27bf4
ComixHe <heyuming@deepin.org>
2024-12-02 17:17:40 +0800

    refactor(uab): remove dependency 'libelf'
    implement needed functions in light_elf
    
    Signed-off-by: ComixHe <heyuming@deepin.org>

---
+++ apps/uab/header/src/utils.h
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#pragma once
+
+#include <utility>
+
+template<typename Func>
+struct defer
+{
+    defer(defer &&newF) = delete;
+    defer(const defer &newF) = delete;
+    defer() = delete;
+    defer &operator=(const defer &) = delete;
+    defer &operator=(defer &&) = delete;
+
+    explicit defer(Func newF)
+        : f(std::move(newF))
+    {
+    }
+
+    ~defer() { f(); }
+
+private:
+    Func f;
+};

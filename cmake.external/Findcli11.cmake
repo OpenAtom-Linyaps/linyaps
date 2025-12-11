78397495f515a9519d3e68535944b92c4496388b
ComixHe <ComixHe1895@outlook.com>
2025-12-11 14:40:22 +0800

    build(deps): bump CLI11 from v2.5.0 to v2.6.1
    Signed-off-by: ComixHe <ComixHe1895@outlook.com>

--- cmake.external/Findcli11.cmake
+++ cmake.external/Findcli11.cmake
@@ -1,4 +1,4 @@
-lls# SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+# SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 

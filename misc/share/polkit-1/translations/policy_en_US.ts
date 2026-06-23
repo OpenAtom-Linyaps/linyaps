23043c57762b7d9acbfb448e3d818ee3597d81c9
dengbo <dengbo@deepin.org>
2026-06-23 20:56:27 +0800

    chore: update policy translations
    Update policy translations.

--- misc/share/polkit-1/translations/policy_en_US.ts
+++ misc/share/polkit-1/translations/policy_en_US.ts
@@ -4,14 +4,64 @@
 	<context>
 		<name>policy</name>
 		<message>
-			<location filename="org.deepin.linglong.PackageManager1.checkAuthentication!message" line="0" />
-			<source>Authentication is required to perform this action</source>
-			<translation>Authentication is required to perform this action</translation>
+			<location filename="org.deepin.linglong.PackageManager1.install!message" line="0" />
+			<source>Authentication is required to install packages</source>
+			<translation>Authentication is required to install packages</translation>
 		</message>
 		<message>
-			<location filename="org.deepin.linglong.PackageManager1.checkAuthentication!description" line="0" />
-			<source>Check Authentication</source>
-			<translation>Check Authentication</translation>
+			<location filename="org.deepin.linglong.PackageManager1.install!description" line="0" />
+			<source>Install packages</source>
+			<translation>Install packages</translation>
+		</message>
+		<message>
+			<location filename="org.deepin.linglong.PackageManager1.update!message" line="0" />
+			<source>Authentication is required to update packages</source>
+			<translation>Authentication is required to update packages</translation>
+		</message>
+		<message>
+			<location filename="org.deepin.linglong.PackageManager1.update!description" line="0" />
+			<source>Update packages</source>
+			<translation>Update packages</translation>
+		</message>
+		<message>
+			<location filename="org.deepin.linglong.PackageManager1.uninstall!message" line="0" />
+			<source>Authentication is required to uninstall packages</source>
+			<translation>Authentication is required to uninstall packages</translation>
+		</message>
+		<message>
+			<location filename="org.deepin.linglong.PackageManager1.uninstall!description" line="0" />
+			<source>Uninstall packages</source>
+			<translation>Uninstall packages</translation>
+		</message>
+		<message>
+			<location filename="org.deepin.linglong.PackageManager1.install-from-file!message" line="0" />
+			<source>Authentication is required to install packages from local files</source>
+			<translation>Authentication is required to install packages from local files</translation>
+		</message>
+		<message>
+			<location filename="org.deepin.linglong.PackageManager1.install-from-file!description" line="0" />
+			<source>Install packages from local files</source>
+			<translation>Install packages from local files</translation>
+		</message>
+		<message>
+			<location filename="org.deepin.linglong.PackageManager1.prune!message" line="0" />
+			<source>Authentication is required to prune package cache</source>
+			<translation>Authentication is required to prune package cache</translation>
+		</message>
+		<message>
+			<location filename="org.deepin.linglong.PackageManager1.prune!description" line="0" />
+			<source>Prune package cache</source>
+			<translation>Prune package cache</translation>
+		</message>
+		<message>
+			<location filename="org.deepin.linglong.PackageManager1.set-configuration!message" line="0" />
+			<source>Authentication is required to modify package manager configuration</source>
+			<translation>Authentication is required to modify package manager configuration</translation>
+		</message>
+		<message>
+			<location filename="org.deepin.linglong.PackageManager1.set-configuration!description" line="0" />
+			<source>Modify package manager configuration</source>
+			<translation>Modify package manager configuration</translation>
 		</message>
 	</context>
 </TS>

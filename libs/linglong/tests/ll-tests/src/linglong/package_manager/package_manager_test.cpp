d5076cb90e0f95ee67feeb2146e295fb17159e44
yuluo-yx <yuluo08290126@gmail.com>
2026-08-11 21:35:40 +0800

    fix(package-manager): validate deferred timeout

---
+++ libs/linglong/tests/ll-tests/src/linglong/package_manager/package_manager_test.cpp
@@ -0,0 +1,67 @@
+/*
+ * SPDX-FileCopyrightText: 2026 UnionTech Software Technology Co., Ltd.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+#include <gtest/gtest.h>
+
+#include "linglong/package_manager/package_manager.h"
+
+#include <QByteArray>
+#include <QTimer>
+
+namespace {
+
+using linglong::service::PackageManager;
+
+class PackageManagerDaemonModeTest : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        hadDeferredTimeout = qEnvironmentVariableIsSet("LINGLONG_DEFERRED_TIMEOUT");
+        deferredTimeout = qgetenv("LINGLONG_DEFERRED_TIMEOUT");
+    }
+
+    void TearDown() override
+    {
+        if (hadDeferredTimeout) {
+            qputenv("LINGLONG_DEFERRED_TIMEOUT", deferredTimeout);
+        } else {
+            qunsetenv("LINGLONG_DEFERRED_TIMEOUT");
+        }
+    }
+
+    static int timerIntervalFor(const QByteArray &value)
+    {
+        qputenv("LINGLONG_DEFERRED_TIMEOUT", value);
+        PackageManager manager{ nullptr, nullptr, nullptr };
+        manager.initDaemonMode();
+        auto *timer = manager.findChild<QTimer *>();
+        EXPECT_NE(timer, nullptr);
+        return timer == nullptr ? -1 : timer->interval();
+    }
+
+    bool hadDeferredTimeout{ false };
+    QByteArray deferredTimeout;
+};
+
+TEST_F(PackageManagerDaemonModeTest, AcceptsPositiveWholeSeconds)
+{
+    EXPECT_EQ(timerIntervalFor("17"), 17'000);
+}
+
+TEST_F(PackageManagerDaemonModeTest, RejectsNonPositiveValues)
+{
+    EXPECT_EQ(timerIntervalFor("0"), 3'600'000);
+    EXPECT_EQ(timerIntervalFor("-1"), 3'600'000);
+}
+
+TEST_F(PackageManagerDaemonModeTest, RejectsTrailingCharactersAndOverflow)
+{
+    EXPECT_EQ(timerIntervalFor("17seconds"), 3'600'000);
+    EXPECT_EQ(timerIntervalFor("2147484"), 3'600'000);
+}
+
+} // namespace

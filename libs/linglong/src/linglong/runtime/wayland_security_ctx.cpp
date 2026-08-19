25ef40fbc2c0ec7b135c3c3509dd427e5be41000
Yuming He <ComixHe1895@outlook.com>
2026-08-19 13:39:37 +0800

    style: update .clang-format
    Signed-off-by: Yuming He <ComixHe1895@outlook.com>

--- libs/linglong/src/linglong/runtime/wayland_security_ctx.cpp
+++ libs/linglong/src/linglong/runtime/wayland_security_ctx.cpp
@@ -62,7 +62,7 @@ WaylandSecurityContextManagerV1::WaylandSecurityContextManagerV1()
         },
         []([[maybe_unused]] void *data,
            [[maybe_unused]] wl_registry *registry,
-           [[maybe_unused]] uint32_t id) {}, // we don't need to handle this
+           [[maybe_unused]] uint32_t id) { }, // we don't need to handle this
     };
 
     auto ret = wl_registry_add_listener(registry, &registry_listener, this);

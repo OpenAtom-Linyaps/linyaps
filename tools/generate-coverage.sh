803166d36dac767869605289e4c2956d8d715167
wurongjie <wurongjie@uniontech.com>
2026-08-19 09:40:49 +0800

    chore(tools/generate-coverage): update license year range to 2023 - 2026
    Addresses review comment on PR.

--- tools/generate-coverage.sh
+++ tools/generate-coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-# SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+# SPDX-FileCopyrightText: 2023 - 2026 UnionTech Software Technology Co., Ltd.
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
